d0b9eb2c6616d5fff4805ff64855becc2b50336d
matias <matias.ferron@ing.austral.edu.ar>
2021-05-29 11:37:00 -0300

    posts

--- src/posts/components/PostCard/PostCard.jsx
+++ src/posts/components/PostCard/PostCard.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import './PostCard.scss';
+import Chip from '@material-ui/core/Chip';
+import Avatar from '@material-ui/core/Avatar';
 
+import ClearIcon from '@material-ui/icons/Clear';
 
 /**
  * @description
@@ -10,15 +13,42 @@ import './PostCard.scss';
  */
 const PostCard = (props) => {
     const {
-        text
+        text,
+        dashboardCards = true
     } = props;
 
+
+
     return (
-        <div>
-            <p>{text || ''}</p>
-        </div>
+        dashboardCards? dashboardCardsComp(text): ownProfileCards(text)
     );
 };
+
+const ownProfileCards = (text) => {
+
+    const handleDelete = (postId) => {
+        // todo
+    }
+
+    // solo puede elimiar post propios
+    return(
+    <div className={'profile-post-card'}>
+        <p>{text || ''}</p>
+        <Chip
+            label={<ClearIcon />}
+            color="secondary"
+        />
+    </div>)
+}
+
+const dashboardCardsComp = (text) => 
+/* solo puede elimiar post propios*/ 
+(
+    <div className={'post-card'}>
+        <Avatar>H</Avatar>
+        <p>{text || ''}</p>
+    </div>
+)
 
 const mapStateToProps = (state) => ({});
 

3b346a4ddc34359e494721659865ec41a2340f66
matias <matias.ferron@ing.austral.edu.ar>
2021-05-29 16:37:51 -0300

    profile initial

--- src/user/components/Profile/Profile.jsx
+++ src/user/components/Profile/Profile.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 import './Profile.scss';
+import PostCard from "../../../posts/components/PostCard/PostCard";
+import {usePrevious} from "../../../utils/hooksRef";
 
 
 /**
@@ -13,14 +15,65 @@ const Profile = (props) => {
 
     } = props;
 
+
+
+    const [postList, setPostList] = useState([]);
+
+    useEffect(()=> {
+        setPostList([
+            {text: 'testing'},
+            {text: 'testing2'},
+            {text: 'testing'},
+            {text: 'testing2'},{text: 'testing'},
+            {text: 'testing2'},{text: 'testing'},
+            {text: 'testing2'},{text: 'testing'},
+            {text: 'testing2'},{text: 'testing'},
+            {text: 'testing2'},{text: 'testing'},
+            {text: 'testing2'},{text: 'testing'},
+            {text: 'testing2'},{text: 'testing'},
+            {text: 'testing2'},{text: 'testing'},
+            {text: 'testing2'},{text: 'testing'},
+            {text: 'testing2'},{text: 'testing'},
+            {text: 'testing2'},
+        ])
+    }, [])
+
     // Ver los datos posts de dicho usuario
     return (
         // y el perfil que estoy viendo es solo texto y un boton de seguir. Entrare con algo como un profile/{id}
-        <div>
-            <p>{'This is Profile'}</p>
+        <div style={{width: '100%', height: '100%'}}>
+            {userProfile({list: postList})}
         </div>
     );
 };
+
+export const userProfile = (userData) => {
+    return (
+        <div className={'user-profile'}>
+            <div className={'profile-column'}>
+                <div className={'profile-img'}>img</div>
+                <div className={'additional-info'}>
+                    email...
+                </div>
+            </div>
+            <div className={'profile-data-column'}>
+                <div className={'title'}>
+                    <h2>Nombre</h2>
+                </div>
+                <div className={'posts-list'}>
+                    <div>
+                        <h3>Posts</h3>
+                    </div>
+                    <div>
+                        {
+                            userData.list.map((x, index) => <PostCard text={x.text} key={index}/>)
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 const mapStateToProps = (state) => ({
 

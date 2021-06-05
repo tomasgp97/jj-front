c1cdb88ff71fd8319fd654fe6c86a111a5a2defc
matias <matias.ferron@ing.austral.edu.ar>
2021-06-04 21:38:24 -0300

    fix proxy deployed?

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,22 +1,45 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const {createProxyMiddleware} = require("http-proxy-middleware");
 
 module.exports = function (app) {
-    app.use(
-        "/api/login",
-        createProxyMiddleware({
-            target: "http://localhost:8080/",
-        })
-    );
-    app.use(
-        "/api/users/register",
-        createProxyMiddleware({
-            target: "http://localhost:8080/",
-        })
-    );
-    app.use(
-        "/api/users",
-        createProxyMiddleware({
-            target: "http://localhost:8080/",
-        })
-    );
+    // todo replace 'localhost' in production to docker img name to test this
+    if (process.env.NODE_ENV === 'production') {
+        app.use(
+            "/api/login",
+            createProxyMiddleware({
+                target: "http://localhost:8080/",
+            })
+        )
+        app.use(
+            "/api/users/register",
+            createProxyMiddleware({
+                target: "http://localhost:8080/",
+            })
+        );
+        app.use(
+            "/api/users",
+            createProxyMiddleware({
+                target: "http://localhost:8080/",
+            })
+        );
+    } else {
+        app.use(
+            "/api/login",
+            createProxyMiddleware({
+                target: "http://localhost:8080/",
+            })
+        );
+        app.use(
+            "/api/users/register",
+            createProxyMiddleware({
+                target: "http://localhost:8080/",
+            })
+        );
+        app.use(
+            "/api/users",
+            createProxyMiddleware({
+                target: "http://localhost:8080/",
+            })
+        );
+    }
+
 };

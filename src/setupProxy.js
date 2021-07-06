88dd1844bf68130d8d58c183f324790065902d93
matias <matias.ferron@ing.austral.edu.ar>
2021-07-05 23:09:37 -0300

    proxy refactor

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,63 +1,40 @@
 const {createProxyMiddleware} = require("http-proxy-middleware");
 
 module.exports = function (app) {
-    // todo replace 'localhost' in production to docker img name to test this
-    if (process.env.NODE_ENV === 'production') {
-        app.use(
-            "/api/login",
-            createProxyMiddleware({
-                target: "http://localhost:8080/",
-            })
-        )
-        app.use(
-            "/api/users/register",
-            createProxyMiddleware({
-                target: "http://localhost:8080/",
-            })
-        );
-        app.use(
-            "/api/users",
-            createProxyMiddleware({
-                target: "http://localhost:8080/",
-            })
-        );
-    } else {
-        app.use(
-            "/api/login",
-            createProxyMiddleware({
-                target: "http://localhost:8080/",
-            })
-        );
-        app.use(
-            "/api/users/register",
-            createProxyMiddleware({
-                target: "http://localhost:8080/",
-            })
-        );
-        app.use(
-            "/api/users",
-            createProxyMiddleware({
-                target: "http://localhost:8080/",
-            })
-        );
-        app.use(
-            "/api/followers",
-            createProxyMiddleware({
-                target: "http://localhost:8080/",
-            })
-        );
-        app.use(
-            "/api/likes",
-            createProxyMiddleware({
-                target: "http://localhost:8082",
-            })
-        );
-        app.use(
-            "/api/posts",
-            createProxyMiddleware({
-                target: "http://localhost:8082",
-            })
-        );
-    }
-
+    app.use(
+        "/api/login",
+        createProxyMiddleware({
+            target: "http://localhost:8080/",
+        })
+    );
+    app.use(
+        "/api/users/register",
+        createProxyMiddleware({
+            target: "http://localhost:8080/",
+        })
+    );
+    app.use(
+        "/api/users",
+        createProxyMiddleware({
+            target: "http://localhost:8080/",
+        })
+    );
+    app.use(
+        "/api/followers",
+        createProxyMiddleware({
+            target: "http://localhost:8080/",
+        })
+    );
+    app.use(
+        "/api/likes",
+        createProxyMiddleware({
+            target: "http://localhost:8082",
+        })
+    );
+    app.use(
+        "/api/posts",
+        createProxyMiddleware({
+            target: "http://localhost:8082",
+        })
+    );
 };
